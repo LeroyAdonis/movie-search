4d3a4584ab7909db4d799815d91f4c99b91ca0a6
Leroy <leroy@Leroys-MacBook-Pro.local>
2019-01-24 10:42:43 +0200

    Edited back to top button - added extra styles and rules

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
+import "./Footer.css";
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.footerContainer = React.createRef();
+  }
+
+  state = { view: "view" };
+
+  componentDidMount() {
+    const footerContainer = this.footerContainer.current;
+    console.log(footerContainer);
+
+    const scrollTop =
+      window.pageYOffset ||
+      (document.documentElement || document.body.parentNode || document.body)
+        .scrollTop;
+
+    console.log(scrollTop);
+
+    if (!scrollTop < 0) {
+      this.setState({ view: "view" });
+    } else {
+      this.setState({ view: "" });
+    }
+  }
+
   scrollUp = () => {
-    var doc = document.documentElement;
-    var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+    const doc = document.documentElement;
+    const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
 
     if (top > 0) {
       window.scrollTo(0, top - 15);
@@ -12,15 +38,20 @@ class Footer extends Component {
   };
   render() {
     return (
-      <div className="ui container">
-        <button
-          style={{ margin: "10px" }}
-          onClick={this.scrollUp}
-          className="ui right floated compact labeled icon button primary"
-        >
-          <i className="arrow circle up icon" />
-          Back to top
-        </button>
+      <div className={this.state.view}>
+        <div className="ui container">
+          <button
+            ref={this.backTop}
+            style={{
+              margin: "10px",
+              backgroundColor: "rgba(0, 181, 173, 0.7)"
+            }}
+            onClick={this.scrollUp}
+            className="ui circular big right floated icon button primary back-top"
+          >
+            <i className="arrow circle up icon" />
+          </button>
+        </div>
       </div>
     );
   }
